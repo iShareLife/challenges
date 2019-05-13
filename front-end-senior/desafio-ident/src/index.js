618fc2a2bdc2c4b6815972237c8fc7bff898c300
Vinicius Moreira <vinicius.ads.mec@gmail.com>
2019-05-13 15:28:03 -0300

    Rota Home base criada

--- front-end-senior/desafio-ident/src/index.js
+++ front-end-senior/desafio-ident/src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'babel-polyfill'
+import 'react-app-polyfill/ie9'
 import * as serviceWorker from './serviceWorker';
+import { Router, Route, Switch } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { LoginCallback } from 'views'
+import { PrivateRoute } from 'components'
+import App from 'App'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const customHistory = createBrowserHistory()
+customHistory.listen(() => window.tracker.pageView())
+
+ReactDOM.render(
+  <Router history={customHistory}>
+    <Switch>
+      <Route path="/login/callback" component={LoginCallback} />
+      <PrivateRoute path="/" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById('root'),
+)
+
 serviceWorker.unregister();
