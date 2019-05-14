b4329308f906ede23ad964667a528b25501a38de
Vinicius Moreira <vinicius.ads.mec@gmail.com>
2019-05-14 13:24:53 -0300

    set mock image na storie

--- front-end-senior/desafio-ident/src/stories/card.stories.js
+++ front-end-senior/desafio-ident/src/stories/card.stories.js
@@ -2,5 +2,6 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import {Card} from 'components';
 import centered from '@storybook/addon-centered/react';
+import imageFile from '../assets/img1.png';
 
-storiesOf('Cards', module).addDecorator(centered).add('Card basic', () => <Card / > );
+storiesOf('Cards', module).addDecorator(centered).add('Card basic', () => <Card img={imageFile} title="Nanotecnologia Aplicada a Implantodontia" / > );
