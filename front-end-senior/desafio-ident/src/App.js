c9cab72f5f6b9db0324b73564e0d5d83f071e836
Vinicius Moreira <vinicius.moreira@stormsec.com.br>
2019-05-13 22:17:39 -0300

    criado tema usando o material theme

--- front-end-senior/desafio-ident/src/App.js
+++ front-end-senior/desafio-ident/src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
 import { Switch } from 'react-router-dom'
-import { PrivateRoute } from 'components'
+import { PrivateRoute, Theme } from 'components'
 import { Home } from 'views'
 import { getToken, setToken, IDentClient } from 'utils'
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.module.css'
 
 class App extends Component {
 
   render() {
-    return (
-        <Switch>
-          <PrivateRoute path="/" exact={true} component={Home} />
-        </Switch>
+      return ( 
+        <React.Fragment>
+          <CssBaseline / >
+          <Theme>
+            <Switch>
+              <PrivateRoute path="/" exact={true} component={Home} />
+            </Switch>
+          </Theme>
+        </React.Fragment>
     )
   }
 }
---
+++ front-end-senior/desafio-ident/src/components/Theme/Theme.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core";
+import MultiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const colors = {
+  defaultColor: "#2962ff",
+  secondaryColor: "#90d528"
+};
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+});
+
+const styles = theme => ({
+  sectionContainer: {
+  }
+});
+
+const Container = props => {
+  const { classes, ...otherProps } = props;
+
+  return (
+    <MultiThemeProvider theme={theme}>
+      <section className={classes.sectionContainer}>{props.children}</section>
+    </MultiThemeProvider>
+  );
+};
+
+Container.propTypes = {
+  children: PropTypes.element.isRequired
+};
+
+export default withStyles(styles)(Container);
