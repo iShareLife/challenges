cc856cf71cd13a5341b2861f597ba07830cc3b7d
Vinicius Moreira <vinicius.ads.mec@gmail.com>
2019-05-15 13:12:36 -0300

    tirado componente não utilizado

--- front-end-senior/desafio-ident/src/components/CarouselCard/CarouselCard.jsx
+++ front-end-senior/desafio-ident/src/components/CarouselCard/CarouselCard.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { ViewsCount, Clock, Card, UpVote, DownVote } from "components";
-import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
