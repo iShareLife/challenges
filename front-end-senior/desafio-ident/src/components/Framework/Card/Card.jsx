be8695f3bd2ace31f5fa99188e857c8a34ccef57
Vinicius Moreira <vinicius.ads.mec@gmail.com>
2019-05-15 13:12:15 -0300

    render storybook

--- front-end-senior/desafio-ident/src/components/Framework/Card/Card.jsx
+++ front-end-senior/desafio-ident/src/components/Framework/Card/Card.jsx
@@ -3,7 +3,8 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import PropTypes from "prop-types";
-import { FabPlay, ViewsCount, UpVote, DownVote, Clock, ButtonBasic } from "components";
+import ButtonBasic  from "../ButtonBasic"; 
+import  FabPlay from "../Icons/FabPlay";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 
