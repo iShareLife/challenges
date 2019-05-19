00d8e9cf0fdd3c697bcd96f0fcca6ef8c8f0cdd7
Vinicius Moreira <vinicius.moreira@stormsec.com.br>
2019-05-19 16:44:00 -0300

    retirado imports nao utilizados

--- front-end-senior/desafio-ident/src/components/Panel/Panel.jsx
+++ front-end-senior/desafio-ident/src/components/Panel/Panel.jsx
@@ -1,20 +1,8 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import PropTypes from "prop-types";
-import {
-  FabPlay,
-  ViewsCount,
-  UpVote,
-  DownVote,
-  Clock,
-  ButtonBasic
-} from "components";
 import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
-import SvgIcon from "@material-ui/core/SvgIcon";
 import Grid from "@material-ui/core/Grid";
 import logo from "../../assets/logo.png";
 
